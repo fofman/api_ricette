4656d322b27a6df70494b944f259792fd8f7d28f
Daniel <daniel.hofman072005@gmail.com>
2024-03-31 20:14:12 +0200

    Modifica nella connessione al db

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => 'root',
+        'username'     => '',
         'password'     => '',
-        'database'     => 'ricette',
+        'database'     => '',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
